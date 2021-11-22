5b47c07b692eba9d44c8349b2727dea20e507f41
theluksteixeira <sobrinho1@gmail.com>
2021-11-22 10:05:33 -0300

    treinamento reactjs

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
+import { App } from "./App";
+
+createServer({
+    routes(){
+        this.namespace = 'api';
+        this.get('/transactions', () => {
+            return [
+                {
+                    id: 1,
+                    title: 'transaction 1',
+                    amount: 400,
+                    type: 'deposit',
+                    category: 'Food',
+                    createAt: new Date()
+                }
+            ]
+        })
+    }
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
